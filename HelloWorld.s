7adef868fd78c826b4f4b09aa2dccbe6293a7170
nellykelly <niceperson787@gmail.com>
2018-10-14 18:16:37 -0700

    first char
    imput first char of ID excluding @

--- HelloWorld.s
+++ HelloWorld.s
@@ -1,13 +1,50 @@
 
         .data
-msg:   .asciiz "1 2 3 4 5 6 7"
+msg:   .asciiz "Koskela, Nelson"
 	.extern foobar 4
 
         .text
         .globl main
-main:   li $v0, 4       # syscall 4 (print_str)
+
+
+
+main:   
+		li $v0, 1
+		li $a0, 0
+		syscall
+
+		li $v0, 1
+		li $a0, 2
+		syscall
+
+		li $v0, 1
+		li $a0, 8
+		syscall
+
+		li $v0, 1
+		li $a0, 3
+		syscall
+
+		li $v0, 1
+		li $a0, 9
+		syscall
+
+		li $v0, 1
+		li $a0, 6
+		syscall
+
+		li $v0, 1
+		li $a0, 6
+		syscall
+
+		li $v0, 1
+		li $a0, 2
+		syscall
+
+		li $v0, 4       # syscall 4 (print_str)
         la $a0, msg     # argument: string
         syscall         # print the string
+
         lw $t1, foobar
         
         jr $ra          # retrun to caller
